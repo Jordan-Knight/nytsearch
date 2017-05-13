8b1d0310b4139e201785c5057f97939143eed460
Jordan Knight <jordanknight@Jordans-MacBook-Pro.local>
2017-05-13 13:26:27 -0500

    things

--- app.js
+++ app.js
@@ -3,22 +3,20 @@
     var startYear = 0;
     var endYear = 0;
     var results = 0;
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + key + "&q=";
-  $("#").on("click", function(){
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + key + "&q=thing";
+  $("#runSearch").on("click", function(event){
 
-      var searchTerm = $("#input").val();
-      var queryURL = queryURL + searchTerm;
-      var results = $("#inputRes").val();
+    //  var searchTerm = $("#input").val();
+     // var queryURL = queryURL + searchTerm;
+    //  var results = $("#inputRes").val();
       
         $.ajax({
             url : queryURL,
             method : "GET",
         }).done(function(response){
-            var results = //number entered;
-            for (i = 0; i < results; i ++){
-                //append each article in the top results area
-                console.log(response);
-            }
+            console.log(response);
+            var results = "";
+
 
         });
     });
